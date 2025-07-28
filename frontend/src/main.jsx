b1bd189e13d5fc4b6ed8499f4ddba500a95db221
Tripplex9 <140714522+Tripplex9@users.noreply.github.com>
2025-07-28 16:48:46 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import '../index.css'; // Corrected to move up to frontend root;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
